e8b761a99b0b42455c2c55d451f43ffcbda67eb2
V-NV <96265492+V-NV@users.noreply.github.com>
2022-05-13 01:54:33 +0500

    feat:'lang change'

--- keyboard/index.js
+++ keyboard/index.js
@@ -1,4 +1,5 @@
 
+   
 
     const cont = document.createElement('div');
     cont.classList.add('cont');
@@ -20,20 +21,22 @@
     En_Ru.classList.add('EnRu');
     En_Ru.innerHTML = 'Не хватает времени, пожалуйста оставьте свой дискорд <br> по нажатию и клику работают только цифры и буквы <br>';
 
+    
 
   
-const keyboardRu = [1105, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61,8];
+const keyboardRu = [/*1105*/49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61,8];
 const keyboardRu2 = [1081,1094, 1091, 1082, 1077, 1085, 1075, 1096, 1097, 1079,1093, 1098,92];
 const keyboardRu3 = [1092, 1099, 1074, 1072, 1087, 1088, 1086,1083, 1076, 1078, 1101]; 
 const keyboardRu4 = [1103, 1095, 1089, 1084, 1080, 1090, 1100, 1073,1102,46];
 
-const keyboardEn = [];
-const keyboardEn2 = [];
-const keyboardEn3 = [];
-const keyboardEn4 = [];
+const keyboardEn = [/*96*/49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61,8];
+const keyboardEn2 = [113,119,101,114,116,121,117,105,111,112,91,93,92];
+const keyboardEn3 = [97,115,100,102,103,104,106,107,108,59,39];
+const keyboardEn4 = [122,120,99,118,98,110,109,44,46,47];
 
 
 function rukla(){
+    document.querySelector('.textarea').focus();
 let out='', out2='', out3= '', out4= '';
 
 for (let i = 0 ; i< keyboardRu.length;i++){
@@ -49,7 +52,7 @@ for (let i = 0 ; i< keyboardRu3.length;i++){
 for (let i = 0 ; i< keyboardRu4.length;i++){
     out4 += '<div class="keys" data="'+keyboardRu4[i]+'">'+String.fromCharCode(`${keyboardRu4[i]}`)+'</div>';
 };
-document.querySelector('.keyboard').innerHTML= out+
+document.querySelector('.keyboard').innerHTML= '<p class="ru" id="lang">ё</p>'+out+
                  '<p class="tab">Tab</p>'+out2+'<p class="del">Del</p>'+
           '<p class="caps">Caps Lock</p>'+out3+'<p class="enter">Enter</p>'+
           '<p class="shift-l">Shift</p>'+out4+'<p class="up">&#x2191</p>'+'<p class="shift-r">Shift</p>'+
@@ -58,10 +61,53 @@ document.querySelector('.keyboard').innerHTML= out+
 }
 rukla();
 
+function enkla(){
+    document.querySelector('.textarea').focus();
+    let out='', out2='', out3= '', out4= '';
+    
+    for (let i = 0 ; i< keyboardEn.length;i++){
+        if(keyboardEn[i] == 8){out+='<p class="backspace">Backspace</p>'} else{
+        out += '<div class="keys" data="'+keyboardEn[i]+'">'+String.fromCharCode(`${keyboardEn[i]}`)+'</div>';
+    }};
+    for (let i = 0 ; i< keyboardEn2.length;i++){
+        out2 += '<div class="keys" data="'+keyboardEn2[i]+'">'+String.fromCharCode(`${keyboardEn2[i]}`)+'</div>';
+    };
+    for (let i = 0 ; i< keyboardEn3.length;i++){
+        out3 += '<div class="keys" data="'+keyboardEn3[i]+'">'+String.fromCharCode(`${keyboardEn3[i]}`)+'</div>';
+    };
+    for (let i = 0 ; i< keyboardEn4.length;i++){
+        out4 += '<div class="keys" data="'+keyboardEn4[i]+'">'+String.fromCharCode(`${keyboardEn4[i]}`)+'</div>';
+    };
+    document.querySelector('.keyboard').innerHTML= '<p class="en" id="lang">`</p>'+out+
+                     '<p class="tab">Tab</p>'+out2+'<p class="del">Del</p>'+
+              '<p class="caps">Caps Lock</p>'+out3+'<p class="enter">Enter</p>'+
+              '<p class="shift-l">Shift</p>'+out4+'<p class="up">&#x2191</p>'+'<p class="shift-r">Shift</p>'+
+    '<p class="ctrl-l">Ctrl</p>'+'<p class="win">Win</p>'+'<p class="alt-l">Alt</p>'+'<p class="space"></p>'+'<p class="alt-r">Alt</p>'+'<p class="ctrl-r">Ctrl</p>'+'<p class="left">&#x2190</p>'+'<p class="down">&#x2193</p>'+'<p class="right">&#x2192</p>';
+    
+    }
+
+
+   
+    document.addEventListener('keydown', (event) => {
+        if (event.ctrlKey && (event.code === 'AltLeft' && lang.textContent == 'ё')) {
+            enkla();
+            
+        }
+        else if (event.ctrlKey && event.code === 'AltLeft'){
+            rukla();
+           
+        }   
+        
+       
+
+      });
+      
+    
+
 /***************lighting letters and numbers on click from keyboard*********************/
 document.onkeypress = function (event)  {
 
-    document.querySelector('.textarea').focus();
+    //document.querySelector('.textarea').focus();
 
    // document.addEventListener('keydown', (event) => {
    //console.log(event.charCode);
@@ -76,14 +122,14 @@ document.addEventListener('keyup', (event) => {
     
     document.querySelectorAll('.keyboard .keys').forEach(function(element){
         element.classList.remove('active')
-        //event.preventDefault();
+        
 })});
 /***************letters and numbers on mouse click*********************/
 document.onclick = function (event)  {
-
+   //o event.preventDefault();
     document.querySelector('.textarea').focus();
 
-let keys = document.querySelectorAll('.keys');
+let keys = document.querySelectorAll('.keys,.ru,.en');
 let display = document.querySelector('.textarea');
 //let clear = document.querySelector('.clear');
 
@@ -93,16 +139,16 @@ for (let key of keys) {
   key.classList.add('active');
   }
 };
-document.querySelectorAll('.keys').forEach(function(element){
+document.querySelectorAll('.keys,.ru,.en').forEach(function(element){
     element.classList.remove('active')
 })
 
 
 
 
-//clear.onclick = function () {
-  //display.textContent = '';
-//};
+/*clear.onmousedown = function () {
+  display.textContent = '';
+};*/
 };
 
 /*
